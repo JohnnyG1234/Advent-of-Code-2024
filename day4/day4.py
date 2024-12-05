d59212b74e9b352271b1c8668da06f28ecddb344
John Groton <john.groton@mymail.champlain.edu>
2024-12-05 11:51:56 -0500

    dead

--- day4/day4.py
+++ day4/day4.py
@@ -1,45 +1,60 @@
 
 
 def find_xmas(index: list[int], puzzle: list[str]):
-    m_index = find_M(index, puzzle)
+    m_indexs = find_letter(index, puzzle, "M")
 
-    if not m_index:
-        return None
+    if m_indexs == []:
+        return 0
     
-    print(m_index)
+    a_indexs = []
+    for m_index in m_indexs:
+        a_indexs.append(find_letter(m_index, puzzle, "A"))
+
+    if a_indexs == []:
+        return 0
     
+    s_indexs = []
+    for a_index in a_indexs:
+        s_indexs.append(find_letter(a_index, puzzle, "S"))
+
+    if s_indexs == []:
+        return 0
+    
+    return  len(s_indexs)    
 
 
-def find_M(index: list[int], puzzle: list[str]):
+def find_letter(pos: list[int], puzzle: list[str], letter: str):
+    indexes: list[list[int]] = []
+
     try:
-        if puzzle[index[0]][index[1] + 1] == "M":
-            print(puzzle[index[0]][index[1] + 1])
-            return [index[0], index[1]]
+        if puzzle[pos[0]][pos[1] + 1] == letter:
+            #print(puzzle[index[0]][index[1] + 1])
+            indexes.append([pos[0], pos[1] + 1])
     except IndexError:
         pass
 
     try:
-        if puzzle[index[0]][index[1] - 1] == "M":
-            print(puzzle[index[0]][index[1] - 1])
-            return [index[0], index[1]]
+        if puzzle[pos[0]][pos[1] - 1] == letter:
+            #print(puzzle[index[0]][index[1] - 1])
+            indexes.append[pos[0], pos[1] - 1]
     except IndexError:
         pass
 
     try:
-        if puzzle[index[0]+1][index[1]] == "M":
-            print(puzzle[index[0]+1][index[1]])
-            return [index[0]+1, index[1]]
+        if puzzle[pos[0]+1][pos[1]] == letter:
+            #print(puzzle[index[0]+1][index[1]])
+            indexes.append[pos[0]+1, pos[1]]
     except IndexError:
         pass
 
     try:
-        if puzzle[index[0]-1][index[1]] == "M":
-            print(puzzle[index[0]-1][index[1]])
-            return [index[0]-1, index[1]]
+        if puzzle[pos[0]-1][pos[1]] == letter:
+            #print(puzzle[index[0]-1][index[1]])
+            indexes.append[pos[0]-1, pos[1]]
     except IndexError:
         pass
 
-    return None
+    return indexes
 
 
 
@@ -47,6 +62,7 @@ def part1():
     data_file = open("data.txt","r")
     data = data_file.readline()
     puzzle: list[str] = []
+    res = 0
 
     while data:
         puzzle.append(data)
@@ -56,7 +72,9 @@ def part1():
     for i in range(len(puzzle)):
         for j in range(len(puzzle[i])):
             if puzzle[i][j] == "X":
-                find_xmas([i,j], puzzle)
+                res += find_xmas([i,j], puzzle)
+    
+    print(res)
 
 
 
